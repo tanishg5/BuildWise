044d745f97b7109ea844b7dad43091ae7eba5040
Tanish Gupta <55tanishqgupta@gmail.com>
2025-06-20 19:55:31 +0530

    commit all changes

--- src/pages/MuscleSpecific.jsx
+++ src/pages/MuscleSpecific.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
 function MuscleSpecific() {
-    const {params} = useParams();
-    console.log(params);
+    const { speciality } = useParams();
+    const {exercise} = useContext(UserContext);
   return (
-    <div>MuscleSpecific</div>
+     <>
+       <div>
+        {
+            Object.entries(exercise.exercises).map(([muscleg , exerciseArray])=>(
+               <div>
+                { muscleg === speciality && (
+                      <div>
+                         {
+                            exerciseArray.map((item  , index)=>(
+                                 <div key = {index}>
+                                    <img src={item.gif_url} alt="" />
+                                    <p>{item.muscle}</p>
+                                    <p>{item.name}</p>
+                                    <p>{item.description1}</p>
+                                    <p>{item.description2}</p>
+                                 </div>
+                            ))
+                         }
+                      </div>
+                )
+                }
+            </div>
+            ))
+        }
+       </div>
+     </>
   )
 }
 
